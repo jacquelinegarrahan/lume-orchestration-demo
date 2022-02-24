b6e7a046f58dd954c91a0f73841a6e96fc55fccf
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-02-23 16:16:24 -0800

    Moving towards injected services

---
+++ slac_services/config.py
@@ -0,0 +1,58 @@
+from dependency_injector import containers, providers
+from dependency_injector.wiring import Provide, inject
+
+from pydantic import BaseSettings
+import yaml
+
+from slac_services.modeling import ModelDBConfig, ModelDB, ModelingService, RemoteModelingService, LocalModelingService
+
+
+class Settings(BaseSettings):
+    model_db: ModelDBConfig
+
+
+class ServiceContainer(containers.DeclarativeContainer):
+
+    config = providers.Configuration()
+
+    model_db = providers.Singleton(
+        ModelDB,
+        db_uri_template=config.model_db.db_uri_template,
+        pool_size= config.model_db.pool_size,
+        user=config.model_db.user,
+        password=config.model_db.password
+    )
+
+    remote_modeling_service = providers.Singleton(
+        RemoteModelingService,
+        model_db=model_db,
+    )
+
+    local_modeling_service = providers.Singleton(
+        LocalModelingService
+    )
+
+
+def parse_config(filepath):
+    """Utility for parsing toml configs
+    
+    """
+    with open(filepath, 'r') as file:
+        config = yaml.safe_load(file)
+
+    model_db = ModelDBConfig(**config["model_db"])
+
+    settings = Settings(model_db=model_db)
+
+    return settings
+
+
+
+def main():
+    container = ServiceContainer()
+    config = parse_config("config.yml")
+    container.config.from_pydantic(config)
+    container.init_resources()
+
+if __name__ == "__main__":
+    main()
--- slac_services/modeling.py
+++ slac_services/modeling.py
@@ -1,27 +1,156 @@
+from contextlib import contextmanager
+from doctest import debug_script
+from re import S
 from slac_services.scheduling import schedule_and_return_run
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.engine.base import Connection
+from sqlalchemy.exc import OperationalError
+from pydantic import BaseSettings
+from string import Template
+
+
+
+class ModelDBConfig(BaseSettings):
+    db_uri_template: str
+    pool_size: int
+    password: str
+    user: str
+
+
+class ModelDB:
+    """
+    Not safe with mutiprocessing at present
+    
+    """
+    def __init__(self, *, db_uri_template, pool_size, user, password):
+        self._db_uri = Template(db_uri_template).substitute(user=user, password=password)
+        self._pool_size = pool_size
+        self._create_engine()
+
+    def _create_engine(self):
+        self._engine = create_engine(self._db_uri, pool_size=self._pool_size)
+    
+    def _connect(self) -> Connection:
+        self._connection = self._engine.connect()
+        # update inspector
+        self._inspector = inspect(self._engine)
+        return self._connection
+
+
+    @contextmanager
+    def connection(self) -> Connection:
+        """This is a context manager bc we want to be able to release connection when finished
+        """
+
+        # check multiprocessing
+        # if pid != pid of current process, create new engine
+
+        # Add cleanup on exit check
+
+        try:
+            yield self._connect()
+
+        finally:
+            self._connection.close()
+
+    def get_model(self, **kwargs):
+        sql = """
+        SELECT model_id 
+        FROM models 
+        WHERE 
+        """
+
+
+        with self.connection as conn:
+            # create inspector
+            model_cols = self._inspector.get_columns("models")
+            col_names = [col["name"] for col in model_cols]
+            
+            kwarg_string = []
+            for kw, value in kwargs.items():
+
+                kwarg_string.append(f"{kw} = {value}")
+
+            sql += kwarg_string.join(" AND ")
+
+            r = conn.execute(sql)
+
+        return r
+
+
+    def save_model():
+        ...
+
+    def save_project():
+        ...
+
+    def save_flow():
+
+        ...
+        
 
 
 class ModelingService():
     ...
+
 
 class LocalModelingService(ModelingService):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._model_registry = {}
 
-    def infer(self, model_id, input_variables):
+    def predict(self, model_id, input_variables):
         self._model_registry[model_id].evaluate(input_variables)
 
 
 class RemoteModelingService():
-    def __init__(self, *args, **kwargs):
+    def __init__(self, model_db: ModelDB, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def infer(self, model_id, input_variables):
-
+    def predict(self, model_id, input_variables):
         data = ...
         flow_name = self._model_registry[model_id]["flow_name"]
         project_name = self._model_registry[model_id]["project_name"]
 
         schedule_and_return_run(flow_name, project_name, data)
-        
+
+    def store_results(self, mongo_service):
+        ...
+
+    def save_model(self):
+        ...
+
+    def load_model(self):
+        ...
+
+
+
+
+"""
+class ModelSerializationService():
+    ...
+    # laboratory
+    # facility
+    # beampath
+    # model id
+
+   # EXTRAS
+    # author
+    # date deployed
+    # version
+    # metadata
+    # flow id 
+
+    # lume-model version -> h5
+    # GH artifacts, filesystem 
+
+    def save_my_model(self, mongodb_service, file_store):
+        # mongodb_service
+
+    def load_model(model_id) -> LUMEModelObject:
+        ...
+
+# LUME-model should use this convention
+model.run()
+model.output()
+"""        
