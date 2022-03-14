66693974af91187733bfc40f3bbc0c9b7c93da68
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-03-14 12:50:33 -0700

    Drop mount path and absolute file from example

--- examples/dockerfile_example/schedule_run.py
+++ examples/dockerfile_example/schedule_run.py
@@ -1,10 +1,8 @@
 from slac_services import service_container
 from slac_services.services.scheduling import MountPoint
 
-
-mount_point = MountPoint(name="fs-test", host_path="/Users/jgarra/sandbox", mount_type="Directory")
     
 scheduler = service_container.prefect_scheduler()
-lume_configuration_file = "/Users/jgarra/sandbox/lume-orchestration-demo/examples/config.yaml"
+lume_configuration_file = "lume-orchestration-demo/examples/config.yaml"
 
-scheduler.schedule_run(flow_id="04728cdd-c6d3-4f65-8cd7-89b902ee6ac7", data= {}, mount_points=[mount_point], lume_configuration_file=lume_configuration_file)
+scheduler.schedule_run(flow_id="04728cdd-c6d3-4f65-8cd7-89b902ee6ac7", data={}, lume_configuration_file=lume_configuration_file)
